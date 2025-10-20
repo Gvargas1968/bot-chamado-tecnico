c3aa6ff07f9807f8c2e4572363ca806b2558db63
Gvargas1968 <gvargas@usp.br>
2025-10-20 14:55:59 -0300

    Clear sensitive data from config_bot_ChamadoBFF.py
    Removed sensitive information and placeholders in the configuration.

--- config_bot_ChamadoBFF.py
+++ config_bot_ChamadoBFF.py
@@ -1,26 +1,26 @@
 # Este arquivo contém seu token do bot
 # Substitua pelo seu token real do @BotFather
-TELEGRAM_TOKEN ="7588285399:AAHd4sjph6s1Ts-uAwHIcFZAxseApZXUEj8"
+TELEGRAM_TOKEN =""
 
 # Configurações do MySQL
 #MYSQL_HOST = "mysql"
-MYSQL_HOST = "10.140.2.157"
-MYSQL_USER = "root"
-MYSQL_PASSWORD = "M$&dff!ch"
+MYSQL_HOST = ""
+MYSQL_USER = ""
+MYSQL_PASSWORD = ""
 MYSQL_DATABASE = "chamados_botbff"
 
 # Defina a senha do administrador para permitir atualização de chamados
-ADMIN_PASSWORD = "1234"
+ADMIN_PASSWORD = ""
 # Senha do administrador
-#ADMIN_PASSWORD = "senha123"
+#ADMIN_PASSWORD = ""
 
 # Técnico responsável
-TECNICO_RESPONSAVEL_ID = 1866083056
-#TECNICO_RESPONSAVEL_ID =  1559668526
+TECNICO_RESPONSAVEL_ID = 
+#TECNICO_RESPONSAVEL_ID =  
 
 # IDs específicos para roteamento por categoria.
 # Substitua pelos IDs de usuários ou grupos do Telegram para cada técnico.
-ID_TECNICO_REDE = 1866083056
-ID_TECNICO_HARDWARE = 1866083056
-ID_TECNICO_SOFTWARE = 1866083056
-ID_TECNICO_IMPRESSORA = 1866083056
+ID_TECNICO_REDE = 
+ID_TECNICO_HARDWARE = 
+ID_TECNICO_SOFTWARE = 
+ID_TECNICO_IMPRESSORA = 
